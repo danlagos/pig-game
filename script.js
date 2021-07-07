918871c7ffe2c57e301edbdba3d78f46b17fb1cb
danlagos <lagos.d@gmail.com>
2021-07-06 20:23:27 -0700

    psuedo code.

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+// on click of roll dice button:  Generate random dice roll (1 to 6)
+// display dice roll.  If (dice roll === 1) {switch player} else
+// {add to current score}.  Display new score
+
+// on click "hold" button : add current score to total score
+// if (score >= 100) {Current player wins} else {switch player}
+
+// on click "new": set all scores to 0.  Set player 1 as starting player
+
+// function to switch players
