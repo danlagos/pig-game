7648fe347e83b30a029118ac5b3056c77bdba5fc
danlagos <lagos.d@gmail.com>
2021-07-07 19:20:58 -0700

    completed changing players feature.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 // Selecting Elements for later use
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -9,7 +11,6 @@ const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-
 const diceEl = document.querySelector('.dice');
 
 // state var
@@ -22,7 +23,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-// on click of roll dice button:  Generate random dice roll (1 to 6)display dice roll.  If (dice roll === 1) {switch player} else {add to current score}.  Display new score
+// on click of roll dice button:  Generate random dice roll (1 to 6)display dice roll.  If (dice roll !== 1) {add to current score} else {switch player}.  Display new score
 
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -35,7 +36,11 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
 
